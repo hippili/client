9480f4bd79d5d5bdf221161c186793bf3859981b
hippili <hippili@unomaha.edu>
2020-09-20 19:19:57 -0500

    third commit

--- clientmsnew/settings.py
+++ clientmsnew/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +32,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.postgres',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,10 +83,13 @@ WSGI_APPLICATION = 'clientmsnew.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CMS',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
+
 
 
 # Password validation
@@ -138,5 +143,7 @@ LOGOUT_REDIRECT_URL = 'home'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
 
 
